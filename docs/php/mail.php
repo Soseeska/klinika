d9d35cb6e4bb35a924f154c6cb2e47bc5ad05973
Soseeska <50383035+Soseeska@users.noreply.github.com>
2021-01-07 13:49:17 +0300

    Update mail.php

--- docs/php/mail.php
+++ docs/php/mail.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'astoyan99@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$6557asa99'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Password = '6557asa99'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
@@ -35,6 +35,6 @@ $mail->AltBody = '';
 if(!$mail->send()) {
     echo 'Error';
 } else {
-    header('location: thank-you.html');
+    header('location: https://soseeska.github.io/klinika/');
 }
 ?>
